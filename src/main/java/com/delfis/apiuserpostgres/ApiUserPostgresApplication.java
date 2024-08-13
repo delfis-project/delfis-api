439412c99c870ebc06d1800631b16525ca17a4aa
joaodinizaraujo <joaovictorclassroom@gmail.com>
2024-08-13 06:24:17 -0300

    subindo projeto base

---
+++ src/main/java/com/delfis/apiuserpostgres/ApiUserPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.delfis.apiuserpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserPostgresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUserPostgresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/delfis/apiuserpostgres/ApiUserPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delfis.apiuserpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserPostgresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
