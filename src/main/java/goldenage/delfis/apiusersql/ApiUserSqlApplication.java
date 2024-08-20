6f887c774a7fef1bb444f627ba93c54482938319
joaodinizaraujo <joaovictorclassroom@gmail.com>
2024-08-20 06:45:41 -0300

    tirando spring security pra testes

--- src/main/java/goldenage/delfis/apiusersql/ApiUserSqlApplication.java
+++ src/main/java/goldenage/delfis/apiusersql/ApiUserSqlApplication.java
@@ -3,7 +3,7 @@ package goldenage.delfis.apiusersql;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ApiUserSqlApplication {
 
     public static void main(String[] args) {
