210983930620ebad6cb90302ac35e207cefe895c
Joao Araujo <joaovictorclassroom@gmail.com>
2024-09-11 15:02:43 -0300

    arrumando nome projeto

--- src/main/java/goldenage/delfis/DelfisApiApplication.java
+++ src/main/java/goldenage/delfis/DelfisApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiDelfisApplication {
+public class DelfisApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiDelfisApplication.class, args);
+        SpringApplication.run(DelfisApiApplication.class, args);
     }
 
 }
