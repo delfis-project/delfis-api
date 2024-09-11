670ee3849052c57cd8410502ebbac90aea6fa88a
João Diniz Araujo <joaovictorclassroom@gmail.com>
2024-09-11 16:55:41 -0300

    Update SecurityConfig.java

--- src/main/java/goldenage/delfis/auth/config/SecurityConfig.java
+++ src/main/java/goldenage/delfis/auth/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()                                       
-                        .requestMatchers("/api/app-user/insert").hasRole("UNLOGGED")
+                        .requestMatchers("/api/app-user/insert").permitAll()
                 )
                 .formLogin(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
