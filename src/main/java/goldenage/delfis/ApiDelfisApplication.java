461d306313279bc5d4f4c99fe8d8539bb4122a3b
joaodinizaraujo <joaovictorclassroom@gmail.com>
2024-09-11 09:52:50 -0300

    corrigindo nome do projeto 1

--- src/main/java/goldenage/delfis/ApiDelfisApplication.java
+++ src/main/java/goldenage/delfis/ApiDelfisApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiUserSqlApplication {
+public class ApiDelfisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiUserSqlApplication.class, args);
+        SpringApplication.run(ApiDelfisApplication.class, args);
     }
 
 }
